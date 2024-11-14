b21043de099b061433b0cd915148249ccf8b5e36
alfar98001 <alfar98001@gmail.com>
2024-11-14 16:01:43 +0300

    Сумма чисел [#505208 #521394]_после корректировки

---
+++ src/test/java/ru/job4j/loop/CounterSumTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterSumTest {
+
+    @Test
+            void whenStart10Finish23Then231() {
+            int start = 10;
+            int finish = 23;
+            int result = CounterSum.sum(start, finish);
+            int expected = 231;
+            assertThat(result).isEqualTo(expected);
+        }
+
+    @Test
+        void whenStart23Finish10Then0() {
+            int start = 23;
+            int finish = 10;
+        int result = CounterSum.sum(start, finish);
+            int expected = 0;
+            assertThat(result).isEqualTo(expected);
+        }
+    }
