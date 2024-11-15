9124ce0496b1802aa15b18a7b98cc58bf965ab80
alfar98001 <alfar98001@gmail.com>
2024-11-15 10:56:17 +0300

    Сумма чисел_тест [#505208 #521394]_после корректировки

--- src/test/java/ru/job4j/loop/CounterSumTest.java
+++ src/test/java/ru/job4j/loop/CounterSumTest.java
@@ -6,20 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CounterSumTest {
 
     @Test
-            void whenStart10Finish23Then231() {
-            int start = 10;
-            int finish = 23;
-            int result = CounterSum.sum(start, finish);
-            int expected = 231;
-            assertThat(result).isEqualTo(expected);
-        }
+    void whenStart10Finish23Then231() {
+        int start = 10;
+        int finish = 23;
+        int result = CounterSum.sum(start, finish);
+        int expected = 231;
+        assertThat(result).isEqualTo(expected);
+    }
 
     @Test
-        void whenStart23Finish10Then0() {
-            int start = 23;
-            int finish = 10;
+    void whenStart23Finish10Then0() {
+        int start = 23;
+        int finish = 10;
         int result = CounterSum.sum(start, finish);
-            int expected = 0;
-            assertThat(result).isEqualTo(expected);
-        }
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
     }
+}
