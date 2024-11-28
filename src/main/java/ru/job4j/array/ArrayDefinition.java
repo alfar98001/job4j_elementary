456e77f4b561116e2361fa5bc12d243547629b0d
alfar98001 <alfar98001@gmail.com>
2024-11-28 15:41:37 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "aid";
+        names[1] = "kira";
+        names[2] = "sam";
+        names[3] = "miho";
+
         System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surnames.length);
+        System.out.println("Размер массива равен: " + names.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println("Размер массива равен: " + names[0]);
+        System.out.println("Размер массива равен: " + names[1]);
+        System.out.println("Размер массива равен: " + names[2]);
+        System.out.println("Размер массива равен: " + names[3]);
+
     }
 
 }
